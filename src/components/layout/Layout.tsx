eb375b11230574e14f66c56f8dbe75fd0fcc27a6
tamim127 <eishatahmedtamim70@gmail.com>
2026-02-05 19:54:44 +0600

    new commit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -36,6 +36,10 @@ const Layout: React.FC<LayoutProps> = ({
 
       <Footer currentLang={currentLang} onNavigate={onNavigate} />
 
+      <div>
+        hello
+      </div>
+
       <ChatWidget />
     </div>
   );
